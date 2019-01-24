e61cb300b328214331559d6684bb9b7c6016b2ee
doris warren, leo mangutov and Nathan Nielson <doris@>
2019-01-24 14:05:57 -0800

    add qu function, consonant function, vowel function

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,50 @@
-var consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y','z']
+// var consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y','z']
 var vowels = ['a', 'e','i','o','u']
 
-function pigLatin(str) {
-  for (var i = 0; i < vowels.length; i += 1) {
-    if (str[0] == vowels[i]) {
-      return str + "way";
-    } else {
-      return str;
-    }
+// function pigLatin(str) {
+//   for (var i = 0; i < vowels.length; i += 1) {
+//     if (str[0] == vowels[i]) {
+//       return str + "way";
+//     }
+//   }
+//     if (str[1] !== vowels[i]) {
+//       return str.slice(2) + str.slice(0,2) + "ay"
+//     }
+//   }
+//   if (str[1] == vowels[i]) {
+//     return str.slice(1) + str.slice(0,1) + "ay"
+//   }
 
+function vowel(word) {
+  if (vowels.includes(word[0])) {
+    return word + "way"
   }
 }
 
+function consonant(word) {
+  for (var i = 0;i < word.length;) {
+    if(!vowels.includes(word[i])) {
+      i += 1;
+    } else {
+      var cons = word.slice(0, [i]);
+      var sliceWord = word.slice(i, word.length);
+      var newWord = sliceWord + cons + "ay"
+      return newWord
+    }
+  }
+}
+
+function qu(word) {
+  var qu = word.slice(0,2)
+  var sliceWord = word.slice(2, word.length)
+  var newWord = sliceWord + qu + "ay"
+  return newWord
+}
+
+// function pigLatin(word) {
+//   vowel(word);
+//   consonant(word);
+// }
 
 
 
@@ -20,43 +53,10 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     var sentence = $("#ans1").val();
-    var result = pigLatin(sentence);
-
-    $("ul").text(result);
+    var result = consonant(sentence);
+    var result2 = vowel(sentence)
+    var result3 = qu(sentence)
+    $("ul").text(result3, result, result2);
 
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (var j = 0; j < str.length; j++) {
-//   if (str[j] == vowels[i]) {
-//
-//   } else {
-//
-//   }
-// }
-
-
-
-
-
-// var sentence = $("#ans1").val().split(" ");
-// console.log(sentence);
-//
-// if (sentence.includes(vowels)) {
-//   alert("hello")
-// } else {
-//
-// }
